652a1eb90f2512ea97bf2af67367de0a48daff83
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-17 19:16:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get '/' do
+  "Hello World!"
 end
+get '/name' do
+  "My name is ."
+end
+get '/hometown' do
+  "My hometown is ."
+end
+get 'favorite-song/' do
+  "My favorite song is ."
+end
+end
+
+# class Example
+#   get '/hello/:id' do
+#     @name = params[:id]
+#     "Hello #{name}!"
+#   end
+# end
